4b273e0d3303e8a630fda9bce096685c9d26fff4
Marcelo S Silva <mrsalustiano@gmail.com>
2024-11-21 00:16:16 -0300

    Primeira Rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WeatherHomeComponent } from './modules/weather/weather-home/weather-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherHomeComponent
   ],
   imports: [
     BrowserModule,
